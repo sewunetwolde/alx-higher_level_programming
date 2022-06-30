6e8e9962196fb6bf45836e5a8b616adc4583db81
sewunetwolde <nekodmos@gmail.com>
2022-06-30 13:39:38 +0300

    rgu

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from sys import argv
-    l = len(argv)
-    print("{:d} {:s}{:s}".format(l - 1, "argument" if l <= 2 else "arguments",
-                                 "." if l == 1 else ":"))
-    for i, s in enumerate(argv):
-        if i > 0:
-            print("{:d}: {:s}".format(i, s))
+    import sys
+    arg_length = len(sys.argv)
+    if arg_length == 1:
+        print("{} arguments.".format(arg_length - 1))
+    elif arg_length == 2:
+        print("{} argument:".format(arg_length - 1))
+    else:
+        print("{} arguments:".format(arg_length - 1))
+
+    for i in range(1, arg_length):
+        print("{}: {}".format(i, sys.argv[i]))
