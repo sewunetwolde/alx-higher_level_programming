2341075a9401b9d81abac714cced1b09a1f951ed
sewunetwolde <nekodmos@gmail.com>
2022-07-04 11:18:44 +0300

    5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,13 +1,7 @@
- #!/usr/bin/python3
+#!/usr/bin/python3
 def no_c(my_string):
-    counts = my_string.count('c')
-    new_string = list(my_string)
-    while counts:
-        new_string.remove('c')
-        counts -= 1
-    counts = my_string.count('C')
-    while counts:
-        new_string.remove('C')
-        counts -= 1
-    new_string = ''.join(new_string)
-    return new_string
+    new = ""
+    for i in my_string:
+        if i is not 'c' and i is not 'C':
+            new += i
+    return 
