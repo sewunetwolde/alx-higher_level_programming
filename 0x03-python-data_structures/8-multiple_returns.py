7f51d7c5e2363313f8d67239ba6634536069b17a
sewunetwolde <nekodmos@gmail.com>
2022-07-04 11:25:32 +0300

    8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if len(sentence) == 0:
-        t = len(sentence), None
-        return t
-    t = len(sentence), sentence[0]
-    return 
+    total = len(sentence)
+    char = sentence[0] if total > 0 else "None"
+    new = total, char
+    return(new)
