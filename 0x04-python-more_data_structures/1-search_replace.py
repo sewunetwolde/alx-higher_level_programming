4e6411de2cc39b8bd58efffb55a6915e5cf48f49
sewunetwolde <nekodmos@gmail.com>
2022-07-06 13:38:23 +0300

    1-search

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    response = [num if num != search else replace for num in my_list]
-    return 
+    return [replace if search == n else n for n in my_list]
