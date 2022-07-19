50e800421bba295d810d5440bcc181b0185e2f59
sewunetwolde <nekodmos@gmail.com>
2022-07-19 15:37:21 +0300

    3

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,33 +1,30 @@
 #!/usr/bin/python3
-"""
-No module imported
-"""
+"""Documentation for a square class"""
 
 
-class Square:
-    """
-    Private instance attribute size
-    public instance method
-    """
+class Square():
+    """Square class for a quadrilateral with four equal sides"""
+
     def __init__(self, size=0):
+        """Sets the initial size values upon instantiation of the class"
+           Throws errors if the value passed in is not an integer
+        Args:
+          size (int, optional): the size of the square object
+        Raises:
+          TypeErrror: when the value passed in is not an integer
+          ValueError: when the value passed in is less than 0
         """
-        private instance attribute
-        parameters
-        ------------------
-        size : integer else TypError
-        if size less than 0, raise value error
-        """
-        self.__size = size
-        try:
-            assert type(size) == int
-        except:
+
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        if size < 0:
+        elif size < 0:
             raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
 
     def area(self):
-        """
-        public instance method
-        returns the current square area
+        """Returns the area of the square
+        Returns:
+           The area of the square
         """
         return self.__size ** 2
